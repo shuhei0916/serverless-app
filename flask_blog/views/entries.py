7792d92a604a5138b32acebae3b646db35f3f46e
shuhei0916 <shuheiito.stc@gmail.com>
2024-03-12 16:20:45 +0900

    added edit.html, chapter6 is done!

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -47,8 +47,25 @@ def show_entry(id):
 
 @app.route('/entries/<int:id>/edit', methods=['GET'])
 def edit_entry(id):
-    # 記事の編集フォームを表示
-    return f'記事{id}の編集フォームを表示'
+    entries = [
+        {
+            'id': 1,
+            'title': '初めての投稿',
+            'text': 'はじめての内容',
+            'created_at': datetime.now()
+        }, 
+        {
+            'id': 2,
+            'title': 'ふたつめの投稿',
+            'text': 'ふたつめの内容',
+            'created_at': datetime.now()
+        },
+    ]
+    entry = None
+    for e in entries:
+        if e['id'] == id:
+            entry = e        
+    return render_template('entries/edit.html', entry=entry)
 
 @app.route('/entries/<int:id>/update', methods=['POST'])
 def update_entry(id):
